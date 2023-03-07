265da290977824ca712ecc418201bf2e991764a3
Adnane Ait Malek <aait-mal@e2r5p3.1337.ma>
2023-03-07 15:00:25 +0100

    all good

--- utils2.c
+++ utils2.c
@@ -6,21 +6,19 @@
 /*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 12:17:22 by aait-mal          #+#    #+#             */
-/*   Updated: 2023/03/06 17:23:11 by aait-mal         ###   ########.fr       */
+/*   Updated: 2023/03/07 14:59:28 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	put_elements(t_map map, void **wall, void **space, void **exit, void **player, void **collectible, void *mlx)
+void	put_elements(t_map map, void **wall, void **space, void **player, void **exit, void **collectible, void *mlx)
 {
-	*wall = mlx_xpm_file_to_image(mlx, "./images/wall.xpm", &map.length, &map.heigth);
-	*space = mlx_xpm_file_to_image(mlx, "./images/space.xpm", &map.length, &map.heigth);
-	// *player = mlx_xpm_file_to_image(mlx, "./images/wall.xpm", &map.length, &map.heigth);
-	// ft_printf("%p\n", *player);
+	*wall = mlx_xpm_file_to_image(mlx, "./xpm/wall2.xpm", &map.length, &map.heigth);
+	*space = mlx_xpm_file_to_image(mlx, "./xpm/ground1.xpm", &map.length, &map.heigth);
+	*player = mlx_xpm_file_to_image(mlx, "./xpm/player/skeleton_down.xpm", &map.length, &map.heigth);
+	*collectible = mlx_xpm_file_to_image(mlx, "./xpm/bone.xpm", &map.length, &map.heigth);
 	(void)exit;
-	(void)player;
-	(void)collectible;
 }
 
 void	print_elements(t_map map, void *mlx, void *mlx_win)
@@ -29,7 +27,7 @@ void	print_elements(t_map map, void *mlx, void *mlx_win)
 	int	y;
 	int	size;
 
-	size = 40;
+	size = 50;
 	y = 0;
 	while (map.map[y])
 	{
@@ -40,8 +38,16 @@ void	print_elements(t_map map, void *mlx, void *mlx_win)
 				mlx_put_image_to_window(mlx, mlx_win, map.wall, x * size, y * size);
 			if (map.map[y][x] == '0')
 				mlx_put_image_to_window(mlx, mlx_win, map.space, x * size, y * size);
-			// if (map.map[y][x] == 'P')
-			// 	mlx_put_image_to_window(mlx, mlx_win, map.player, x * size, y * size);
+			if (map.map[y][x] == 'C')
+			{
+				mlx_put_image_to_window(mlx, mlx_win, map.space, x * size, y * size);
+				mlx_put_image_to_window(mlx, mlx_win, map.collectible, x * size, y * size);
+			}
+			if (map.map[y][x] == 'P')
+			{
+				mlx_put_image_to_window(mlx, mlx_win, map.space, x * size, y * size);
+				mlx_put_image_to_window(mlx, mlx_win, map.player, x * size, y * size);
+			}
 			x++;
 		}
 		y++;
@@ -58,7 +64,7 @@ void	print_map(int fd, char *name)
 	reopen(fd, name);
 	map.map = get_map(fd, name, &map.length, &map.heigth);
 	display_map(map);
-	size_of_block = 40;
+	size_of_block = 50;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, map.length * size_of_block,
 			map.heigth * size_of_block, "So Long");
